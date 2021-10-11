ec9be4c1fad4e28552cbe360fce982f9a98fc072
Evelyn Cleto <67518341+EvelynCleto@users.noreply.github.com>
2021-10-11 09:40:48 -0300

    Update index.php

--- xampp/htdocs/app_help_desk/index.php
+++ xampp/htdocs/app_help_desk/index.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <link rel="stylesheet" type="text/css" href="https://evelyncleto.github.io/HelpDesk.github.io/">
+    <link rel="stylesheet"  href="https://evelyncleto.github.io/HelpDesk.github.io/">
     <meta charset="utf-8" />
     <title>App Help Desk</title>
 
